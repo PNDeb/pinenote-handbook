37c303639b49ff17724cb7b1cd901b21b129b6e7
Maximilian Weigand <mweigand@mweigand.net>
2025-01-26 18:14:08 +0100

    hopefully fix some issues with autostarting the greeter

--- files/etc/greeter/greeter.sh
+++ files/etc/greeter/greeter.sh
@@ -4,6 +4,8 @@
 count_file="$HOME/.config/pinenote/greeter_count"
 # we do not start the greeter if this file exists
 disable_file="$HOME/.config/pinenote/disable_greeter"
+# this file should lead the Pinenote GNOME extension to disable the overview
+disable_overview_file="$HOME/.config/pinenote/do_not_show_overview"
 
 #
 test -e "${count_file}" || echo 0 > "${count_file}"
@@ -15,14 +17,23 @@ echo "Content: ${content}"
 if test "$content" -eq 0; then
 	content=1;
 	test -e "${disable_file}" && rm "${disable_file}"
+	# make sure the overview stays disabled
+	touch "${disable_overview_file}"
+
 	echo "${content}" > "${count_file}"
 elif test "${content}" -eq 1; then
 	content=2;
 	test -e "${disable_file}" && rm "${disable_file}"
+	# make sure the overview stays disabled
+	touch "${disable_overview_file}"
+
 	echo "${content}" > "${count_file}"
 elif test "${content}" -eq 2; then
 	content=3;
 	test -e "${disable_file}" && rm "${disable_file}"
+	# make sure the overview stays disabled
+	touch "${disable_overview_file}"
+
 	echo "${content}" > "${count_file}"
 else
 	echo "Greeter started enough times. Will not delete the disable file"
@@ -34,12 +45,14 @@ test -e "${disable_file}" && exit
 # use gnome-help-based yelp to show the help page
 yelp /etc/greeter/html/index.html&
 
+# we need to wait a bit until we delete lock files because it is possible that
+# the gnome extension is too slow to check for the files
+sleep 5
+
 # check for configuration directory
 test -d $HOME/.config/pinenote || mkdir $HOME/.config/pinenote
 
 # touching this file will prevent the help from being shown on next boot/login
 touch "${disable_file}"
 
-# this file should lead the Pinenote GNOME extension to disable the overview
-disable_overview_file="$HOME/.config/pinenote/do_not_show_overview"
 test -e ${disable_overview_file} && rm "${disable_overview_file}"
